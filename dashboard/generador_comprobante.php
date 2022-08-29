85e4ab6635f04d6cf0ff3c3dd6045ae5d144b743
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-29 15:34:11 -0500

    logo pdf 20

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -31,7 +31,7 @@ function Footer()
     /* Arial italic 8 */
     $this->SetFont('Arial','I',8);
     /* Page number */
-    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+    $this->Cell(0,10,'Hoja '.$this->PageNo().'/{nb}',0,0,'C');
 }
 }
 
@@ -40,6 +40,15 @@ $pdf = new PDF();
 $pdf->AliasNbPages();
 $pdf->AddPage();
 $pdf->SetFont('Times','',12);
+$string="El señor José Perez tiene mucho poder.";
+$pdf->MultiCell(180,8,$string);
 
 $pdf->Output();
 ?>
+
+<strong>Estimado participante:</strong>
+<p>Por medio de la presente, le agradecemos el haber participado en la convocatoria del <strong>Premio Estatal de la Juventud 2022</strong> en la ciudad de Zacatecas, Zac.</p>
+<p>El Gobierno del Estado de Zacatecas agradece tu participación.</p>
+<p>ATENTAMENTE</p>
+<p><strong>ING. MAURICIO ACEVEDO RODRÍGUEZ</p>
+<p>DIRECTOR GENERAL<br>DEL INJUVENTUD</strong></p>
