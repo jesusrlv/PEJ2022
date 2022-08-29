41e660a1ba25825054b174f7846bdd541c8b4859
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-29 15:14:12 -0500

    logo pdf 17

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -20,7 +20,7 @@ function Header()
     // Logo
     $this->Image('../img/RESP_logo.png',81,27,54);
   
-    $this->Cell(90,10,'Premio Estatal de la Juventud 2022',0,0,'C');
+    $this->Cell(81,10,'Premio Estatal de la Juventud 2022',0,0,'C');
     
 }
 /* Page footer */
