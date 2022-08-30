bc15577a7f000938fd3e09968d2abd5f1f2a035d
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-30 08:59:23 -0500

    pdf 30082022

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -8,61 +8,6 @@ session_start();
 
 require('prcd/fpdf/fpdf.php');
 
-class PDF extends FPDF
-{
-/* Page header */
-function Header()
-{
-    
-    $this->SetFont('Arial','B',15);
-    /* Move to the right */
-    $this->Cell(60);
-    // Logo
-    $this->Image('../img/RESP_logo.png',81,27,54);
-  
-    $this->Cell(81,10,utf8_decode('Constancia de participación'),0,0,'C');
-    
-}
-/* Page footer */
-function Footer()
-{
-    /* Position at 1.5 cm from bottom */
-    $this->SetY(-15);
-    /* Arial italic 8 */
-    $this->SetFont('Arial','I',8);
-    /* Page number */
-    $this->Cell(0,10,'Hoja '.$this->PageNo().'/{nb}',0,0,'C');
-}
-}
-
-/* Instanciation of inherited class */
-$pdf = new PDF();
-$pdf->AliasNbPages();
-$pdf->AddPage();
-$pdf->SetFont('Times','',12);
-$string1="Estimado participante:";
-$string2="Por medio de la presente, le agradecemos el haber participado en la convocatoria del Premio Estatal de la Juventud 2022 en la ciudad de Zacatecas, Zac. El Gobierno del Estado de Zacatecas agradece tu participación. ATENTAMENTE ING. MAURICIO ACEVEDO RODRÍGUEZ DIRECTOR GENERAL DEL INJUVENTUD";
-$pdf->Cell(0,100,$string1);
-$pdf->Cell(0,120,$string2);
-
-$pdf->Output();
-?>
-
-<!-- <strong>Estimado participante:</strong>
-<p>Por medio de la presente, le agradecemos el haber participado en la convocatoria del <strong>Premio Estatal de la Juventud 2022</strong> en la ciudad de Zacatecas, Zac.</p>
-<p>El Gobierno del Estado de Zacatecas agradece tu participación.</p>
-<p>ATENTAMENTE</p>
-<p><strong>ING. MAURICIO ACEVEDO RODRÍGUEZ</p>
-<p>DIRECTOR GENERAL<br>DEL INJUVENTUD</strong></p> -->
-
-<!-- codigo para generar pdf -->
-
-<?php
-
-//request id
-
-
-require 'lib/fpdf/fpdf.php';
 
 $pdf = new FPDF();
 $pdf->AddPage();
