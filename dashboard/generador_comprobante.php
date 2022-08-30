07764c6ff3c20d425a5162eea18af88a482ad78f
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-30 13:53:06 -0500

    texto para oficio 11

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -8,72 +8,6 @@ session_start();
 
 require('prcd/fpdf/fpdf.php');
 
-// class PDF extends FPDF
-// {
-// /* Page header */
-// function Header()
-// {
-    
-//     $this->SetFont('Arial','B',15);
-//     /* Move to the right */
-//     $this->Cell(60);
-//     // Logo
-//     $this->Image('../img/RESP_logo.png',81,27,54);
-  
-//     $this->Cell(81,10,utf8_decode('Constancia de participación'),0,0,'C');
-    
-// }
-// /* Page footer */
-// function Footer()
-// {
-//     /* Position at 1.5 cm from bottom */
-//     $this->SetY(-15);
-//     /* Arial italic 8 */
-//     $this->SetFont('Arial','I',8);
-//     /* Page number */
-//     $this->Cell(0,10,'Hoja '.$this->PageNo().'/{nb}',0,0,'C');
-// }
-// }
-
-// /* Instanciation of inherited class */
-// $pdf = new PDF();
-// $pdf->AliasNbPages();
-// $pdf->AddPage();
-// // $pdf->Image('../img/RESP_logo.png',10,12,30,0,'','http://www.fpdf.org');
-// $pdf->SetFont('Arial','C',10);
-// $pdf->Cell(72,10,'',0, 0, 'C');
-// $pdf->Cell(90,10,'',0, 1, 'R');
-
-// $pdf->Multicell(190,4,'Por medio de la presente, el Instituto de la Juventud del Estado de Zacatecas, a través de su Director General, el Ing. Mauricio Acevedo Rodríguez, reconoce a usted su participación como candidato(a) a recibir el Premio Estatal de la Juventud en su edición 2022.
-
-// Su postulación es fundamental para la construcción de un Estado más próspero, incluyente, democrático e igualitario. Gracias a su intervención, recordamos lo importante que es reconocer el talento y la trayectoria de las y los jóvenes en nuestra Entidad. Los principios rectores de la Nueva Gobernanza, nos obligan a impulsar y generar los espacios oportunos para que las juventudes puedan alcanzar sus objetivos, desarrollarse de manera integral y vivir en bienestar. No sólo representas uno de los cimientos más importantes de la sociedad, también eres protagonista del presente y agente estratégico para la transformación nacional.
-
-// Continúa abriendo brechas, rompiendo estigmas y creciendo, ¡Tú talento y capacidades, no tienen límites!
-
-// En la ciudad de Zacatecas, Zac., noviembre de 2022.',0,'J',0);
-
-// $pdf->Cell(300,5,'En la ciudad de Zacatecas, Zac., noviembre de 2022.',0,1,'R');
-// $pdf->Cell(320,5,'DIRECTOR GENERAL INSTITUTO DE LA JUVENTUD DEL ESTADO DE ZACATECAS',0,1,'R');
-
-// $pdf->Output();
-?>
-
-<!-- <strong>Estimado participante:</strong>
-<p>Por medio de la presente, le agradecemos el haber participado en la convocatoria del <strong>Premio Estatal de la Juventud 2022</strong> en la ciudad de Zacatecas, Zac.</p>
-<p>El Gobierno del Estado de Zacatecas agradece tu participación.</p>
-<p>ATENTAMENTE</p>
-<p><strong>ING. MAURICIO ACEVEDO RODRÍGUEZ</p>
-<p>DIRECTOR GENERAL<br>DEL INJUVENTUD</strong></p> -->
-
-<!-- codigo para generar pdf -->
-
-<?php
-
-//request id
-
-
-// require 'lib/fpdf/fpdf.php';
-
 $pdf = new FPDF();
 $pdf->AddPage();
 $pdf->SetFont('Arial','B',10);
@@ -105,7 +39,4 @@ Comité Organizador Congreso Internacional sobre Gestión de las Organizaciones'
 );
 
 $pdf->Output();
-
-
-
 ?>
