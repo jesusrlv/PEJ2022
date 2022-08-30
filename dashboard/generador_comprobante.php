073641f10a0d1b58b50be9b4fcea60165ba03824
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-30 14:02:14 -0500

    texto para oficio 15

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -8,22 +8,60 @@ session_start();
 
 require('prcd/fpdf/fpdf.php');
 
-$pdf = new FPDF();
+// $pdf = new FPDF();
+// $pdf->AddPage();
+// $pdf->SetFont('Arial','B',10);
+// $pdf->Image('../img/RESP_logo.png',10,12,30,0,'','http://www.fpdf.org');
+// $pdf->Cell(72,10,'Universidad Autónoma de Zacatecas',0, 0, 'C');
+// $pdf->Cell(90,10,'Oficio de Aceptación de Ponencia',0, 1, 'R');
+
+// $pdf->Multicell(300,9,'Estimado(a) participante,
+// Por medio de la presente, el Instituto de la Juventud del Estado de Zacatecas, a través de su Director General, el Ing. Mauricio Acevedo Rodríguez, reconoce a usted su participación como candidato(a) a recibir el Premio Estatal de la Juventud en su edición 2022.
+
+// Su postulación es fundamental para la construcción de un Estado más próspero, incluyente, democrático e igualitario. Gracias a su intervención, recordamos lo importante que es reconocer el talento y la trayectoria de las y los jóvenes en nuestra Entidad. Los principios rectores de la Nueva Gobernanza, nos obligan a impulsar y generar los espacios oportunos para que las juventudes puedan alcanzar sus objetivos, desarrollarse de manera integral y vivir en bienestar. No sólo representas uno de los cimientos más importantes de la sociedad, también eres protagonista del presente y agente estratégico para la transformación nacional.
+
+// Continúa abriendo brechas, rompiendo estigmas y creciendo, ¡Tú talento y capacidades, no tienen límites!
+// ',1,'J',0
+
+// );
+
+// $pdf->Output();
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+    // Logo
+    $this->Image('../img/RESP_logo.png',10,8,33);
+    // Arial bold 15
+    $this->SetFont('Arial','B',15);
+    // Movernos a la derecha
+    $this->Cell(80);
+    // Título
+    $this->Cell(30,10,'Constancia de participación',0,0,'C');
+    // Salto de línea
+    $this->Ln(20);
+}
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,'Page '.$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+// Creación del objeto de la clase heredada
+$pdf = new PDF();
+$pdf->AliasNbPages();
 $pdf->AddPage();
-$pdf->SetFont('Arial','B',10);
-$pdf->Image('../img/RESP_logo.png',10,12,30,0,'','http://www.fpdf.org');
-$pdf->Cell(72,10,'Universidad Autónoma de Zacatecas',0, 0, 'C');
-$pdf->Cell(90,10,'Oficio de Aceptación de Ponencia',0, 1, 'R');
-
-$pdf->Multicell(300,7,'Estimado(a) participante,
-Por medio de la presente, el Instituto de la Juventud del Estado de Zacatecas, a través de su Director General, el Ing. Mauricio Acevedo Rodríguez, reconoce a usted su participación como candidato(a) a recibir el Premio Estatal de la Juventud en su edición 2022.
-
-Su postulación es fundamental para la construcción de un Estado más próspero, incluyente, democrático e igualitario. Gracias a su intervención, recordamos lo importante que es reconocer el talento y la trayectoria de las y los jóvenes en nuestra Entidad. Los principios rectores de la Nueva Gobernanza, nos obligan a impulsar y generar los espacios oportunos para que las juventudes puedan alcanzar sus objetivos, desarrollarse de manera integral y vivir en bienestar. No sólo representas uno de los cimientos más importantes de la sociedad, también eres protagonista del presente y agente estratégico para la transformación nacional.
-
-Continúa abriendo brechas, rompiendo estigmas y creciendo, ¡Tú talento y capacidades, no tienen límites!
-',1,'J',0
-
-);
-
+$pdf->SetFont('Times','',12);
+$pdf->Cell(0,10,'Por medio de la presente, el Instituto de la Juventud del Estado de Zacatecas, a través de su Director General, el Ing. Mauricio Acevedo Rodríguez, reconoce a usted su participación como candidato(a) a recibir el Premio Estatal de la Juventud en su edición 2022.',0,1);
 $pdf->Output();
+
 ?>
