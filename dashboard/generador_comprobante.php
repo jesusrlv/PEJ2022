d11c226f2d68f59e7bf7f13547e9989c73b796f5
jesusrlv <jesusrlvrojo@gmail.com>
2022-08-30 13:58:04 -0500

    texto para oficio 14

--- dashboard/generador_comprobante.php
+++ dashboard/generador_comprobante.php
@@ -15,7 +15,7 @@ $pdf->Image('../img/RESP_logo.png',10,12,30,0,'','http://www.fpdf.org');
 $pdf->Cell(72,10,'Universidad Autónoma de Zacatecas',0, 0, 'C');
 $pdf->Cell(90,10,'Oficio de Aceptación de Ponencia',0, 1, 'R');
 
-$pdf->Multicell(200,7,'Estimado(a) participante,
+$pdf->Multicell(300,7,'Estimado(a) participante,
 Por medio de la presente, el Instituto de la Juventud del Estado de Zacatecas, a través de su Director General, el Ing. Mauricio Acevedo Rodríguez, reconoce a usted su participación como candidato(a) a recibir el Premio Estatal de la Juventud en su edición 2022.
 
 Su postulación es fundamental para la construcción de un Estado más próspero, incluyente, democrático e igualitario. Gracias a su intervención, recordamos lo importante que es reconocer el talento y la trayectoria de las y los jóvenes en nuestra Entidad. Los principios rectores de la Nueva Gobernanza, nos obligan a impulsar y generar los espacios oportunos para que las juventudes puedan alcanzar sus objetivos, desarrollarse de manera integral y vivir en bienestar. No sólo representas uno de los cimientos más importantes de la sociedad, también eres protagonista del presente y agente estratégico para la transformación nacional.
